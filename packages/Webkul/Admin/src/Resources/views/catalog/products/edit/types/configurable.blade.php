2ac634e9b7cf93f8cdd3e5d20434805bc0c62751
amit kumar laravel <amitkumar.laravel358@webkul.in>
2024-11-13 19:18:32 +0530

    Fixed Issue #10308

--- packages/Webkul/Admin/src/Resources/views/catalog/products/edit/types/configurable.blade.php
+++ packages/Webkul/Admin/src/Resources/views/catalog/products/edit/types/configurable.blade.php
@@ -1480,14 +1480,16 @@
 
             created() {
                 let inventories = {};
-                
-                if (Array.isArray(this.variant.inventories)) {
-                    this.variant.inventories.forEach((inventory) => {
-                        inventories[inventory.inventory_source_id] = inventory.qty;
-                    });
 
-                    this.variant.inventories = inventories; 
-                }
+                this.inventorySources.forEach((source) => {
+                    const inventory = Array.isArray(this.variant.inventories)
+                        ? this.variant.inventories.find(inventory => inventory.inventory_source_id === source.id)
+                        : null;
+
+                    inventories[source.id] = inventory ? (inventory.qty || 0) : 0;
+                });
+
+                this.variant.inventories = inventories;
             },
 
             mounted() {
